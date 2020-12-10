dc22e520bda84bd2913902e02593fdad1739128c
Anurag Harsh <harshanurag97@gmail.com>
2020-12-10 08:14:24 +0530

    Update checkIfDoorIsOpenOrClose.py

--- checkIfDoorIsOpenOrClose.py
+++ checkIfDoorIsOpenOrClose.py
@@ -1,4 +1,6 @@
-#Given n doors and n persons. The doors are numbered 1 to n and persons are given id’s numbered 1 to n. Each door can have only 2 status open and closed. Initially all the doors #have status closed. Find the final status of all the doors if a person changes the current status of all the doors, i.e. if status open then change to status closed and vice #versa, for which he is authorized. A person with id ‘i’ is authorized to change the status of door numbered ‘j’ if ‘j’ is a multiple of ‘i’.
+#Given n doors and n persons. The doors are numbered 1 to n and persons are given id’s numbered 1 to n. Each door can have only 2 status open and closed. Initially all the doors 
+#have status closed. Find the final status of all the doors if a person changes the current status of all the doors, i.e. if status open then change to status closed and vice 
+#versa, for which he is authorized. A person with id ‘i’ is authorized to change the status of door numbered ‘j’ if ‘j’ is a multiple of ‘i’.
 
 #Approach:
 #Check if count of divisors is odd or even.
